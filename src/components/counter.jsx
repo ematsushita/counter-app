a503640deb8d4806b60c7d23855a21b646d02778
Elissa Matsushita <elissamatsushita@Elissas-MacBook.local>
2020-04-16 10:57:17 -0700

    counter app exercise

---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = { 
+    count: 0
+  };
+
+  render() { 
+    let classes = this.getBadgeClasses();
+
+    return (
+      <>
+        <span className={ classes }>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+
+      </>
+    );
+  }
+  
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const {count} = this.state
+    return count === 0 ? 'Zero' : count;
+  }
+
+}
+ 
+export default Counter;
