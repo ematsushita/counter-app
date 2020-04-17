4624c4691c2f5df18a84979f8b4bec43082460b5
Elissa Matsushita <elissamatsushita@Elissas-MacBook.local>
2020-04-17 16:10:40 -0700

    components and lifecycle hooks

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = { 
-    count: 0
-  };
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount(){
+    console.log("Counter - Unmount")
+  }
+
 
   render() { 
+    console.log("Counter - Rendered")
     let classes = this.getBadgeClasses();
+    let disabledBool = this.getDisabledBoolean();
 
     return (
-      <>
-        <span className={ classes }>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
 
-      </>
+      <div className="container row">
+        <div className="col-1">
+          <span className={ classes }>{this.formatValue()}</span>
+        </div>
+        <div className="col">
+          <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">+</button>
+          <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-2" disabled={disabledBool}>-</button>
+          <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm">X</button>
+        </div>
+      </div>
     );
   }
   
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1})
-  }
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const {count} = this.state
-    return count === 0 ? 'Zero' : count;
+  getDisabledBoolean() {
+    return this.props.counter.value === 0;
+  }
+
+  formatValue() {
+    const {value} = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 
 }
